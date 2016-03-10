d2eba14d17643f39f5baee151602b9487553308f
moolamdeepthi <deepthi.moolam@D469.blr.velankani.com>
2016-03-10 12:18:51 +0530

    adding project demo with sdnbooks code

---
+++ demo/src/demo/Sdnbooks.java
@@ -0,0 +1,47 @@
+package demo;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Sdnbooks {
+	public class DifferentWebsites {
+	public	 WebDriver driver = new FirefoxDriver();
+	public String expectedTitle = null;
+	public String actualTitle = null;
+
+	@BeforeMethod      
+
+	public void page() throws AWTException{
+		Robot newtab = new Robot();
+		newtab.keyPress(KeyEvent.VK_CONTROL);
+		newtab.keyPress(KeyEvent.VK_T);
+		newtab.keyRelease(KeyEvent.VK_CONTROL);
+		newtab.keyRelease(KeyEvent.VK_T);
+	}
+	@Test(priority=0)
+	public void amazon(){
+		driver.get("http://www.amazon.com");	
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("SDN books");
+		driver.findElement(By.xpath(".//*[@id='nav-search']/form/div[2]/div/input")).click();
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(scrFile, new File("Z:\\scr\\amazon.jpG"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}}
+}
